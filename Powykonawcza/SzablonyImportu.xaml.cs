b0d12e0d59cf654c16712affe4ad7416f83cd740
Piotr Dusinski <pdusp@tlen.pl>
2021-02-11 13:16:54 +0100

    od p.s. poprawka kolejna wersja

--- Powykonawcza/SzablonyImportu.xaml.cs
+++ Powykonawcza/SzablonyImportu.xaml.cs
@@ -19,7 +19,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Powykonawcza.Model.Szablon
+using Powykonawcza.Model.Szablon;
 
 namespace Powykonawcza
 {
