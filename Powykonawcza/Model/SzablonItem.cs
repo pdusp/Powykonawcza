3c13004722601600098b862766c97f4971cd7a43
Piotr Stęclik <piotr.steclik@isukces.com>
2021-02-11 12:19:02 +0100

    a to czyni propertisy bardziej przejrzyste

--- Powykonawcza/Model/SzablonItem.cs
+++ Powykonawcza/Model/SzablonItem.cs
@@ -7,72 +7,56 @@ namespace Powykonawcza.Model.Szablon
     [Serializable]
     public class SzablonItem : INotifyPropertyChanged
     {
-        private string _nazwa;
-        private bool _import;
-        private string _inne;
         public SzablonItem(string nazwa, bool import, string inne)
         {
-            this._nazwa = nazwa;
-            this._import = import;
-            this._inne = inne;
-        }
-        public string nazwa
-        {
-            get
-            {
-                return _nazwa;
-            }
-            set
-            {
-                _nazwa = value;
-                OnPropertyRaised();
-            }
-        }
-        public bool import
-        {
-            get
-            {
-                return _import;
-            }
-            set
-            {
-                _import = value;
-                OnPropertyRaised();
-            }
-        }
-        public string inne
-        {
-            get
-            {
-                return _inne;
-            }
-
-            set
-            {
-                _inne = value;
-                OnPropertyRaised();
-            }
-
+            _nazwa  = nazwa;
+            _import = import;
+            _inne   = inne;
         }
 
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyRaised([CallerMemberName] string propertyname=null)
+        protected void SetAndNotify<T>(ref T backField,
+            T value,
+            [CallerMemberName] string propertyName = null)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
-            }
+            if (Equals(backField, value))
+                return;
+            backField = value;
+            var handler = PropertyChanged;
+            if (handler == null) return;
+            var propertyChangedEventArgs = new PropertyChangedEventArgs(propertyName);
+            handler(this, propertyChangedEventArgs);
+        }
+
+        private void OnPropertyRaised([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
         }
 
 
         public static SzablonItem SampleInstance { get; } = new SzablonItem("Blabla", true, "inne");
 
+        public string nazwa
+        {
+            get => _nazwa;
+            set => SetAndNotify(ref _nazwa, value);
+        }
+
+        public bool import
+        {
+            get => _import;
+            set => SetAndNotify(ref _import, value);
+        }
+
+        public string inne
+        {
+            get => _inne;
+            set => SetAndNotify(ref _inne, value);
+        }
+
+        private string _nazwa;
+        private bool _import;
+        private string _inne;
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
-
-
-
-
-
-
 }
