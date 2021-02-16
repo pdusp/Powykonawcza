524438c740c275844e9f708eeb304a77b6126237
pdusp <pdusp@tlen.pl>
2021-02-16 14:35:47 +0100

    Interface IImport.cs

---
+++ Powykonawcza/Services/IImport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powykonawcza.Services
+{
+    public interface IImport
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="percent">0..100</param>
+        void ReportProgress(double percent);
+    }
+}
---
+++ Powykonawcza/Services/Import.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powykonawcza.Services
+{
+    class Import : IImport
+    {
+        void IImport.ReportProgress(double percent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
